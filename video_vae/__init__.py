8d1ad63715e0a145fca421a7a106ff69f06acc72
koparkrea <koparkrea@gmail.com>
2024-11-14 19:21:02 +0900

    add

--- video_vae/__init__.py
+++ video_vae/__init__.py
@@ -1,4 +1,4 @@
 from .modeling_loss import LPIPSWithDiscriminator
 from .modeling_causal_vae import CausalVideoVAE
-from .modeling_block import CausalVaeDecoder, CausalVaeEncoder
+from .modeling_enc_dec import CausalVaeDecoder, CausalVaeEncoder
 from .causal_video_vae_wrapper import CausalVideoVAELossWrapper
