e4f441368c4d30ff22a3cd620711efc65761cee2
koparkrea <koparkrea@gmail.com>
2024-11-14 19:01:59 +0900

    add

--- scripts/train_pyramid_flow_ours.sh
+++ scripts/train_pyramid_flow_ours.sh
@@ -20,8 +20,8 @@ NUM_FRAMES=16         # e.g., 16 for 5s, 32 for 10s
 
 # For the 768p version, make sure to add the args:  --gradient_checkpointing
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_processes $GPUS \
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_processes $GPUS \
     train/train_pyramid_flow_ours.py \
     --num_workers 8 \
     --task t2v \
