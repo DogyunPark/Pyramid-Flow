01f5e0d75dabf46bbfd1ac6c74571214f15de6e9
koparkrea <koparkrea@gmail.com>
2024-11-14 17:35:28 +0900

    add

---
+++ tools/__init__.py
@@ -0,0 +1,1 @@
+from .inference import load_data_prompts
--- train/train_pyramid_flow_ours.py
+++ train/train_pyramid_flow_ours.py
@@ -571,7 +571,10 @@ def main(args):
         if args.output_dir and accelerator.is_main_process:
             with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
                 f.write(json.dumps(log_stats) + "\n")
-            
+        
+        if 1:
+            pass
+
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
