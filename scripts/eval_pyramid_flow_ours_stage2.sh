d2bec762838227e7e96da5a74db10432c204c427
koparkrea <koparkrea@gmail.com>
2024-11-19 13:11:50 +0900

    add

--- scripts/eval_pyramid_flow_ours_stage2.sh
+++ scripts/eval_pyramid_flow_ours_stage2.sh
@@ -25,9 +25,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python \
     train/eval_pyramid_flow_ours_stage2.py \
     --num_workers 8 \
     --task t2v \
-    --use_fsdp \
     --use_temporal_causal \
-    --fsdp_shard_strategy $SHARD_STRATEGY \
     --interp_condition_pos \
     --load_text_encoder \
     --model_name $MODEL_NAME \
