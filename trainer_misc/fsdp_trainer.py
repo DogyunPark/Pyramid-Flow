e69118f83c06db41462822074350a3b10fef2682
koparkrea <koparkrea@gmail.com>
2024-11-21 13:05:02 +0900

    add

--- trainer_misc/fsdp_trainer.py
+++ trainer_misc/fsdp_trainer.py
@@ -96,7 +96,7 @@ def train_one_epoch_with_fsdp(
                 image = runner.generate_video(
                     prompt=text[1],
                     input_image=video[1:2, :, :1],
-                    num_inference_steps=[60, 60, 60],
+                    num_inference_steps=[20, 20, 20],
                     output_type="pil",
                     save_memory=True,
                     guidance_scale=6.0
