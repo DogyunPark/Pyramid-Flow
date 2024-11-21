16587fb453e82e861114895cfb8f750613a16c00
koparkrea <koparkrea@gmail.com>
2024-11-21 19:57:22 +0900

    add

--- scripts/train_pyramid_flow_ours.sh
+++ scripts/train_pyramid_flow_ours.sh
@@ -58,4 +58,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_process
     --gradient_checkpointing \
     --use_flash_attn \
     --temporal_autoregressive \
-    --trilinear_interpolation
+    --trilinear_interpolation \
+    --condition_original_image \
