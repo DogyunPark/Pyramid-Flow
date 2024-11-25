0b221336ede0d9b70a71ce4a9f93605048490e4a
koparkrea <koparkrea@gmail.com>
2024-11-25 14:39:42 +0900

    add

--- scripts/train_pyramid_flow_ours_stage2.sh
+++ scripts/train_pyramid_flow_ours_stage2.sh
@@ -23,7 +23,7 @@ NUM_FRAMES=16         # e.g., 16 for 5s, 32 for 10s
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python \
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_processes $GPUS \
     train/train_pyramid_flow_ours_stage2.py \
-    --num_workers 8 \
+    --num_workers 4 \
     --task t2v \
     --use_fsdp \
     --fsdp_shard_strategy $SHARD_STRATEGY \
