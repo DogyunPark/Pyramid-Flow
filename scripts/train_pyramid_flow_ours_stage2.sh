b2c374c3a6d748520027217307377e60cf19524a
koparkrea <koparkrea@gmail.com>
2024-11-25 21:16:09 +0900

    add

--- scripts/train_pyramid_flow_ours_stage2.sh
+++ scripts/train_pyramid_flow_ours_stage2.sh
@@ -61,4 +61,3 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_process
     --condition_original_image \
     --num_frames 49 \
     --deterministic_noise \
-    --dit_pretrained_weight /data/cvpr25/Pyramid-Flow/result/stage1/checkpoint-1000-2/pytorch_model_fsdp.bin \
