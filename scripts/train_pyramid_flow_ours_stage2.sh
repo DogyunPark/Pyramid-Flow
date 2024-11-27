fe12bf49bffcbc0c105b55738c064e7d782ad130
koparkrea <koparkrea@gmail.com>
2024-11-27 20:23:40 +0900

    add

--- scripts/train_pyramid_flow_ours_stage2.sh
+++ scripts/train_pyramid_flow_ours_stage2.sh
@@ -57,7 +57,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --multi_gpu --num_process
     --save_ckpt_freq 1 \
     --load_vae \
     --gradient_checkpointing \
-    --downsample_latent \
     --num_frames 49 \
     --deterministic_noise \
     --condition_original_image \
