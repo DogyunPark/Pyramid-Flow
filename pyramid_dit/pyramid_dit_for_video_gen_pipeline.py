b6735ea63d5d1a2073424f03cd0341ef1f0cc7ff
koparkrea <koparkrea@gmail.com>
2024-11-29 02:59:21 +0900

    add

--- pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
+++ pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
@@ -1141,8 +1141,8 @@ class PyramidDiTForVideoGeneration:
             height //= 2
             width //= 2
             x = rearrange(x, 'b c t h w -> (b t) c h w')
-            #if self.use_gaussian_filter:
-            #    x = self.gaussian_filter(x)
+            if self.use_gaussian_filter:
+                x = self.gaussian_filter(x)
             x = torch.nn.functional.interpolate(x, size=(height, width), mode='bilinear')
             x = rearrange(x, '(b t) c h w -> b c t h w', t=temp)
             vae_latent_list.append(x)
