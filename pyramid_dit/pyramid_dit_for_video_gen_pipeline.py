1eceb667b236a8175e9b46d745bcb69aa199503a
koparkrea <koparkrea@gmail.com>
2024-11-28 12:09:42 +0900

    add

--- pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
+++ pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
@@ -706,6 +706,117 @@ class PyramidDiTForVideoGeneration:
         return length_list
 
     @torch.no_grad()
+    def add_pyramid_noise_ours3(
+        self, 
+        latents_list,
+        upsample_vae_latent_list,
+        sample_ratios=[1, 1, 1],
+    ):
+        """
+        add the noise for each pyramidal stage
+            noting that, this method is a general strategy for pyramid-flow, it 
+            can be used for both image and video training.
+            You can also use this method to train pyramid-flow with full-sequence 
+            diffusion in video generation (without using temporal pyramid and autoregressive modeling)
+
+        Params:
+            latent_list: [low_res, mid_res, high_res] The vae latents of all stages
+            sample_ratios: The proportion of each stage in the training batch
+        """
+        noise = torch.randn_like(latents_list[-1])
+        device = noise.device
+        dtype = latents_list[-1].dtype
+        t = noise.shape[2]
+
+        stages = len(self.stages)
+        tot_samples = noise.shape[0]
+        assert tot_samples % (int(sum(sample_ratios))) == 0
+        assert stages == len(sample_ratios)
+        
+        # To calculate the padding batchsize and column size
+        batch_size = tot_samples // int(sum(sample_ratios))
+        column_size = int(sum(sample_ratios))
+        
+        column_to_stage = {}
+        i_sum = 0
+        for i_s, column_num in enumerate(sample_ratios):
+            for index in range(i_sum, i_sum + column_num):
+                column_to_stage[index] = i_s
+            i_sum += column_num
+
+        noisy_latents_list = []
+        ratios_list = []
+        targets_list = []
+        timesteps_list = []
+        training_steps = self.scheduler.config.num_train_timesteps
+
+        # from low resolution to high resolution
+        for index in range(column_size):
+            i_s = column_to_stage[index]
+            start_sigma = self.scheduler.start_sigmas[i_s]
+            end_sigma = self.scheduler.end_sigmas[i_s]
+            
+            if i_s == 0:
+                start_point = latents_list[-1][index::column_size]
+                start_point = rearrange(start_point, 'b c t h w -> (b t) c h w')
+                start_point = torch.nn.functional.interpolate(start_point, size=(start_point.shape[-2] // (2*2), start_point.shape[-1] // (2*2)), mode='bilinear')
+                start_point = rearrange(start_point, '(b t) c h w -> b c t h w', t=1)
+            else:
+                # Get the upsampled latent
+                last_clean_latent = latents_list[-1][index::column_size]
+                last_clean_latent = rearrange(last_clean_latent, 'b c t h w -> (b t) c h w')
+                last_clean_latent = torch.nn.functional.interpolate(last_clean_latent, size=(last_clean_latent.shape[-2] // (2*(3-i_s)), last_clean_latent.shape[-1] // (2*(3-i_s))), mode='bilinear')
+                start_point = torch.nn.functional.interpolate(last_clean_latent, size=(last_clean_latent.shape[-2] * 2, last_clean_latent.shape[-1] * 2), mode='nearest')
+                start_point = rearrange(start_point, '(b t) c h w -> b c t h w', t=1)
+            
+            if i_s == stages - 1:
+                end_point = latents_list[-1][index::column_size]   # [bs, c, t, h, w]
+            else:
+                clean_latent = latents_list[-1][index::column_size]   # [bs, c, t, h, w]
+                clean_latent = rearrange(clean_latent, 'b c t h w -> (b t) c h w')
+                end_point = torch.nn.functional.interpolate(clean_latent, size=(clean_latent.shape[-2] // (2*(2-i_s)), clean_latent.shape[-1] // (2*(2-i_s))), mode='bilinear')
+                end_point = rearrange(end_point, '(b t) c h w -> b c t h w', t=1)
+
+            if self.condition_original_image:
+                original_latent_condition = latents_list[i_s+1][index::column_size]
+                original_latent_condition = original_latent_condition[:,:,0].unsqueeze(2)
+                noise_ratio2 = torch.rand(size=(batch_size,), device=device) / 3
+                noise_ratio2 = noise_ratio2[:, None, None, None, None]
+                original_latent_condition = noise_ratio2 * torch.randn_like(original_latent_condition) + (1 - noise_ratio2) * original_latent_condition
+
+            # To sample a timestep
+            u = compute_density_for_timestep_sampling(
+                weighting_scheme='random',
+                batch_size=batch_size,
+                logit_mean=0.0,
+                logit_std=1.0,
+                mode_scale=1.29,
+            )
+
+            indices = (u * training_steps).long()   # Totally 1000 training steps per stage
+            indices = indices.clamp(0, training_steps-1)
+            timesteps = self.scheduler.timesteps_per_stage[i_s][indices].to(device=device)
+            ratios = self.scheduler.sigmas_per_stage[i_s][indices].to(device=device)
+
+            while len(ratios.shape) < start_point.ndim:
+                ratios = ratios.unsqueeze(-1)
+
+            # interpolate the latent
+            noisy_latents = ratios * start_point + (1 - ratios) * end_point
+
+            # [stage1_latent, stage2_latent, ..., stagen_latent], which will be concat after patching
+            if self.condition_original_image:
+                noisy_latents_list.append([original_latent_condition, noisy_latents.to(dtype)])
+            else:
+                noisy_latents_list.append([noisy_latents.to(dtype)])
+                    
+            ratios_list.append(ratios.to(dtype))
+            timesteps_list.append(timesteps.to(dtype))
+            targets_list.append(start_point - end_point)     # The standard rectified flow matching objective
+
+        return noisy_latents_list, ratios_list, timesteps_list, targets_list
+    
+    @torch.no_grad()
     def add_pyramid_noise_with_temporal_pyramid(
         self, 
         latents_list,
@@ -1117,7 +1228,7 @@ class PyramidDiTForVideoGeneration:
         #if use_temporal_pyramid:
         #    noisy_latents_list, ratios_list, timesteps_list, targets_list = self.add_pyramid_noise_with_temporal_pyramid(vae_latent_list, self.sample_ratios)
         if self.use_perflow:
-            noisy_latents_list, ratios_list, timesteps_list, targets_list = self.add_pyramid_noise_ours(vae_latent_list, upsample_vae_latent_list, self.sample_ratios)
+            noisy_latents_list, ratios_list, timesteps_list, targets_list = self.add_pyramid_noise_ours3(vae_latent_list, upsample_vae_latent_list, self.sample_ratios)
         else:
             noisy_latents_list, ratios_list, timesteps_list, targets_list = self.add_pyramid_noise_ours2(vae_latent_list, upsample_vae_latent_list, self.sample_ratios)
 
@@ -1827,10 +1938,14 @@ class PyramidDiTForVideoGeneration:
         latent_temp = int(self.num_frames // self.vae.config.downsample_scale + 1)
 
         # Prepare the condition latents
-        stage_latent_condition = rearrange(input_image, 'b c t h w -> (b t) c h w')
+        #stage_latent_condition = rearrange(input_image, 'b c t h w -> (b t) c h w')
+        #stage_latent_condition = torch.nn.functional.interpolate(stage_latent_condition, size=(height//(2**(stage_num-1)), width//(2**(stage_num-1))), mode='bilinear')
+        #stage_latent_condition = rearrange(stage_latent_condition, '(b t) c h w -> b c t h w', t=1)
+        stage_latent_condition = (self.vae.encode(input_image.to(self.vae.device, dtype=self.vae.dtype), temporal_chunk=False, tile_sample_min_size=1024).latent_dist.sample() - self.vae_shift_factor) * self.vae_scale_factor  # [b c t h w] 
+        stage_latent_condition = rearrange(stage_latent_condition, 'b c t h w -> (b t) c h w')
         stage_latent_condition = torch.nn.functional.interpolate(stage_latent_condition, size=(height//(2**(stage_num-1)), width//(2**(stage_num-1))), mode='bilinear')
         stage_latent_condition = rearrange(stage_latent_condition, '(b t) c h w -> b c t h w', t=1)
-        stage_latent_condition = (self.vae.encode(stage_latent_condition.to(self.vae.device, dtype=self.vae.dtype), temporal_chunk=False, tile_sample_min_size=1024).latent_dist.sample() - self.vae_shift_factor) * self.vae_scale_factor  # [b c t h w] 
+        
         #stage_latent_condition = (self.vae.encode(stage_latent_condition.to(self.vae.device, dtype=self.vae.dtype), temporal_chunk=False, tile_sample_min_size=1024).latent_dist.sample() - self.vae_video_shift_factor) * self.vae_video_scale_factor  # [b c t h w] 
         #if self.condition_original_image:
         if not self.downsample_latent:
