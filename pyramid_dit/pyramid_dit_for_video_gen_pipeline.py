3529f6890d10a7a73741c57eebaa1dc7526638a6
koparkrea <koparkrea@gmail.com>
2024-11-29 13:05:17 +0900

    add

--- pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
+++ pyramid_dit/pyramid_dit_for_video_gen_pipeline.py
@@ -845,14 +845,14 @@ class PyramidDiTForVideoGeneration:
                 #start_point_0 = start_sigma * start_point_0 + (1 - start_sigma) * end_point_0
                 start_point_0 = end_point_0
                 #start_point_1 = start_sigma * start_point_1 + (1 - start_sigma) * end_point_1
-                start_point = start_point_1 + latents_list[i_s][index::column_size]
+                #start_point = start_point_1 + latents_list[i_s][index::column_size]
                 #end_point_0 = end_sigma * start_point_0 + (1 - end_sigma) * end_point_0
                 #end_point_1 = end_sigma * start_point_1 + (1 - end_sigma) * end_point_1
                 
-                # start_point_0 = rearrange(start_point_0, 'b c t h w -> (b t) c h w')
-                # start_point_0 = torch.nn.functional.interpolate(start_point_0, size=(start_point_1.shape[-2], start_point_1.shape[-1]), mode='nearest')
-                # start_point_0 = rearrange(start_point_0, '(b t) c h w -> b c t h w', t=t)
-                # start_point = start_point_1 + start_point_0
+                start_point_0 = rearrange(start_point_0, 'b c t h w -> (b t) c h w')
+                start_point_0 = torch.nn.functional.interpolate(start_point_0, size=(start_point_1.shape[-2], start_point_1.shape[-1]), mode='nearest')
+                start_point_0 = rearrange(start_point_0, '(b t) c h w -> b c t h w', t=t)
+                start_point = start_point_1 + start_point_0
 
                 # end_point_0 = rearrange(end_point_0, 'b c t h w -> (b t) c h w')
                 # end_point_0 = torch.nn.functional.interpolate(end_point_0, size=(end_point_1.shape[-2], end_point_1.shape[-1]), mode='nearest')
@@ -888,7 +888,11 @@ class PyramidDiTForVideoGeneration:
                 # start_point_1 = rearrange(start_point_1, '(b t) c h w -> b c t h w', t=t)
                 # start_point = start_point_2 + start_point_1
 
-                start_point = start_point_2 + latents_list[i_s][index::column_size]
+                start_point_1 = latents_list[i_s][index::column_size]
+                start_point_1 = rearrange(start_point_1, 'b c t h w -> (b t) c h w')
+                start_point_1 = torch.nn.functional.interpolate(start_point_1, size=(start_point_2.shape[-2], start_point_2.shape[-1]), mode='nearest')
+                start_point_1 = rearrange(start_point_1, '(b t) c h w -> b c t h w', t=t)
+                start_point = start_point_2 + start_point_1
 
                 # end_point_0 = rearrange(end_point_0, 'b c t h w -> (b t) c h w')
                 # end_point_0 = torch.nn.functional.interpolate(end_point_0, size=(end_point_1.shape[-2], end_point_1.shape[-1]), mode='nearest')
